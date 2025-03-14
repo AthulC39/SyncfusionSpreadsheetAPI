388b185f5dc2cb79cab342ad2b108b843fd08807
AthulC39 <athulcwork@gmail.com>
2025-03-14 11:15:32 -0400

    changed frontend url

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("https://elemmentmmpfrontenddemo.onrender.com") // Replace with your React app URL
+        policy.WithOrigins("https://elemment-mmp-frontend-demo-zum5.vercel.app") // Replace with your React app URL
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
