2b03c1aec2a1ec84cf4532f74366a2f0899bf7c5
AthulC39 <athulcwork@gmail.com>
2025-03-17 07:50:40 -0400

    add

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("https://elemment-mmp-frontend-demo-zum5.vercel.app", "http://localhost:3002")
+        policy.WithOrigins("https://elemment-mmp-frontend-demo.vercel.app", "http://localhost:3002")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
